55d825bc955f762e7dabeef350c591fbb19f4093
Zhengyang Kris Weng <wengmister@gmail.com>
2026-02-11 02:08:22 -0600

    Fixed marker visualization and added chiral colors

--- hand_tracking_sdk_ros2/adapters.py
+++ hand_tracking_sdk_ros2/adapters.py
@@ -120,34 +120,46 @@ def to_marker_array(
 
     marker_array = MarkerArray()
 
-    points_marker = Marker()
-    points_marker.header = make_header(stamp=stamp, frame_id=frame_id)
-    points_marker.ns = f"{side_ns}_landmarks"
-    points_marker.id = 0
-    points_marker.type = Marker.SPHERE_LIST
-    points_marker.action = Marker.ADD
-    points_marker.scale.x = 0.012
-    points_marker.scale.y = 0.012
-    points_marker.scale.z = 0.012
     if frame.side == HandSide.LEFT:
-        points_marker.color.r = 1.0
-        points_marker.color.g = 0.5
-        points_marker.color.b = 0.2
+        color_r = 0.2
+        color_g = 0.4
+        color_b = 1.0
     else:
-        points_marker.color.r = 0.2
-        points_marker.color.g = 0.6
-        points_marker.color.b = 1.0
-    points_marker.color.a = 1.0
-    points_marker.points = [Point(x=x, y=y, z=z) for x, y, z in points]
+        color_r = 1.0
+        color_g = 0.2
+        color_b = 0.2
+
+    for idx, (x, y, z) in enumerate(points):
+        joint_marker = Marker()
+        joint_marker.header = make_header(stamp=stamp, frame_id=frame_id)
+        joint_marker.ns = f"{side_ns}_landmark_spheres"
+        joint_marker.id = idx
+        joint_marker.type = Marker.SPHERE
+        joint_marker.action = Marker.ADD
+        joint_marker.pose.position.x = x
+        joint_marker.pose.position.y = y
+        joint_marker.pose.position.z = z
+        joint_marker.pose.orientation.w = 1.0
+        joint_marker.scale.x = 0.012
+        joint_marker.scale.y = 0.012
+        joint_marker.scale.z = 0.012
+        joint_marker.color.r = color_r
+        joint_marker.color.g = color_g
+        joint_marker.color.b = color_b
+        joint_marker.color.a = 1.0
+        marker_array.markers.append(joint_marker)
 
     line_marker = Marker()
     line_marker.header = make_header(stamp=stamp, frame_id=frame_id)
     line_marker.ns = f"{side_ns}_bones"
-    line_marker.id = 1
+    line_marker.id = 1000
     line_marker.type = Marker.LINE_LIST
     line_marker.action = Marker.ADD
     line_marker.scale.x = 0.004
-    line_marker.color = points_marker.color
+    line_marker.color.r = color_r
+    line_marker.color.g = color_g
+    line_marker.color.b = color_b
+    line_marker.color.a = 1.0
 
     for idx_a, idx_b in BONE_EDGES:
         pa = points[idx_a]
@@ -155,7 +167,6 @@ def to_marker_array(
         line_marker.points.append(Point(x=pa[0], y=pa[1], z=pa[2]))
         line_marker.points.append(Point(x=pb[0], y=pb[1], z=pb[2]))
 
-    marker_array.markers.append(points_marker)
     marker_array.markers.append(line_marker)
     return marker_array
 
